0d04ec867fd3497681928c28231d704821923c33
Weiqi Feng <fengweiqi@sjtu.edu.cn>
2020-05-25 19:43:22 +0800

    Occupy charts2 of first dash board

---
+++ static/js/economic.js
@@ -0,0 +1,16 @@
+/**
+ * Economic graph
+ * 
+ * Author: Weiqi Feng
+ * Email:  fengweiqi@sjtu.edu.cn
+ * Date:   May 25, 2020  
+ * Copyright 2019 Vic
+ */
+(function () {
+    "use strict";
+    window.addEventListener("load", initialize);
+    /* Add a function that will be called when window is loaded */
+    function initialize() {
+        console.log("Vic hello world!");
+    }
+})()
