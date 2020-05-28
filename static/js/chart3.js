1809bd1f62dd4d5c4dc9113172805499f73c837c
Weiqi Feng <fengweiqi@sjtu.edu.cn>
2020-05-28 09:28:28 +0800

    Add chart3

---
+++ static/js/chart3.js
@@ -0,0 +1,15 @@
+/**
+ * Chart 3
+ * 
+ * Author: Weiqi Feng
+ * Email:  fengweiqi@sjtu.edu.cn
+ * Date:   May 28, 2020
+ * Copyright 2019 Vic   
+ */
+(function() {
+    "use strict";
+    window.addEventListener("load", initialize);
+    function initialize() {
+        console.log(1);
+    }
+})()
